4454df46c057585bc6414a87449157a8ddfad0da
Abdul-Samad-Noorwala <samadsalman14@gmail.com>
2023-12-01 15:37:31 +0500

    Learn Variables

---
+++ variable.dart
@@ -0,0 +1,30 @@
+void main() {
+  print("My macOS Setup");
+  print("---------------\n");
+
+  print("Macbook Pro 15-inch, 2018 \n");
+
+  String detail = "Processor";
+  print(detail);
+
+  print("2.2 GHz 6-Core Intel Core i7\n");
+
+  String details = "Graphics";
+  print(details);
+
+  print("Intel UHD Graphics 630 1536 MB\n");
+
+  String det = "Memory";
+  print(det);
+
+  print("16 GB 2400 MHz DDR4\n");
+
+  String bud = "Best Macbook In Budget Of";
+  print(bud);
+  int amount = 250000;
+  print('\n$amount');
+
+  print("Reliable Machine Ever");
+  bool ff = true;
+  print(ff);
+}
