f552f392bd32af7bed2c4cf68936ea1d0967ee68
Abdul-Samad-Noorwala <samadsalman14@gmail.com>
2023-12-05 15:05:10 +0500

    Learn List

---
+++ list.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("IPHONIC STOCK");
+  print("--------------");
+  List<String> stock = [
+    "iPhone X",
+    "iPhone XR",
+    "iPhone XS",
+    "iPhone XS Max",
+  ];
+  List<int> storage = [64, 128, 256, 512];
+  print(stock[1]);
+  print(storage[1]);
+  print(stock[3]);
+  print(storage[3]);
+  print(stock[0] + " is sold");
+}
