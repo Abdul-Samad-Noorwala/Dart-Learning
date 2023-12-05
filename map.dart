773008ee3a56e97abab841662f7cb5bb297aada6
Abdul-Samad-Noorwala <samadsalman14@gmail.com>
2023-12-05 17:26:42 +0500

    Learn Maps

---
+++ map.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<Map<String, String>> stock = [
+    {
+      "iPhone 14 Pro Max": "Storage 128gb",
+      "PTA Approved": "Deep Purple Waterpack Complete Box",
+    },
+    {
+      "iPhone 15 Pro Max": "Storage 256gb",
+      "Non PTA": "Natural Titanium",
+      "Factory Unlock": "Hong Kong Variant",
+    },
+  ];
+  print(stock[0]);
+  print(stock[1]);
+}
