73358b7895a7876f0f9bc3fa70cebdf9f68096d0
Abdul-Samad-Noorwala <samadsalman14@gmail.com>
2023-12-17 03:03:33 +0500

    Learn Functions

---
+++ function.dart
@@ -0,0 +1,55 @@
+void main() {
+  print("Learning What Is Function And How To Use Function In Dart");
+  print("We Are Studying From Sulanza Tech");
+  print("Charges = 10K");
+  iphonic();
+  motors();
+  icars();
+  apple();
+  markhor();
+}
+
+void iphonic() {
+  String stock = "Brand New Stock Arrived At iPhonic";
+  print(stock);
+  print("For Daily Stock Updates Join Our Whatsapp Group");
+  int num = 3343800608;
+  print(num);
+  print("Drop Us Hi At The Given Number For The Group Link");
+}
+
+void motors() {
+  String stock = "Brand New Stock Arrived At Rohaan Motors";
+  print(stock);
+  print("For Daily Stock Updates Join Our Whatsapp Group");
+  int num = 3343800608;
+  print(num);
+  print("Drop Us Hi At The Given Number For The Group Link");
+}
+
+void icars() {
+  String stock = "Brand New Stock Arrived At iCars";
+  print(stock);
+  print("For Daily Stock Updates Join Our Whatsapp Group");
+  int num = 3343800608;
+  print(num);
+  print("Drop Us Hi At The Given Number For The Group Link");
+}
+
+void apple() {
+  String stock = "Brand New Stock Arrived At Apple Cellar";
+  print(stock);
+  print("For Daily Stock Updates Join Our Whatsapp Group");
+  int num = 3343800608;
+  print(num);
+  print("Drop Us Hi At The Given Number For The Group Link");
+}
+
+void markhor() {
+  String stock = "Brand New Stock Arrived At Markhor Motors";
+  print(stock);
+  print("For Daily Stock Updates Join Our Whatsapp Group");
+  int num = 3343800608;
+  print(num);
+  print("Drop Us Hi At The Given Number For The Group Link");
+}
